b20d6b7e10362471d198ac0741024bf0f0507304
Alec Gorge <alecgorge@gmail.com>
2019-03-20 15:03:54 -0700

    Clean up interface

--- Source/ThreadQueue.swift
+++ Source/ThreadQueue.swift
@@ -96,15 +96,17 @@ public class ThreadQueue: Foundation.Thread {
         enqueue(block)
     }
     
-    final public func sync(_ block: @escaping () -> ()) {
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        enqueue {
-            block()
-            semaphore.signal()
+    final public func sync(_ block: () -> ()) {
+        withoutActuallyEscaping(block) { escapableBlock in
+            let semaphore = DispatchSemaphore(value: 0)
+            
+            enqueue {
+                escapableBlock()
+                semaphore.signal()
+            }
+            
+            semaphore.wait()
         }
-        
-        semaphore.wait()
     }
 
     /**
